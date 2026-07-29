71cf25d8db62508bc44298c592e0951ba1d7628d
UnlessDahell <148602200+UnlessDahell@users.noreply.github.com>
2026-07-30 06:59:46 +0700

    Fix link for 'A dusty trip' in Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -29,7 +29,7 @@ local Loaders = {
     },
     {
         Ids = {16389395869, 16389398622, 17527914941}, -- A dusty trip
-        Link = "https://raw.githubusercontent.com/UnlessDahell/Zelo/refs/heads/main/ADT"
+        Link = "https://raw.githubusercontent.com/UnlessDahell/Zelo/refs/heads/main/adustytrip"
     },
     {
         Ids = {286090429}, -- Arsenal
